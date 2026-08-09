05d156e45ddeea0039e8d9cacc859cb04141a0e9
Kotryos <13850665+Kotryos@users.noreply.github.com>
2026-08-09 19:10:38 +0200

    Scaffold Spring Boot 3.5 application on Java 21

---
+++ src/main/java/dev/kotryos/minischeduler/MiniSchedulerApplication.java
@@ -0,0 +1,12 @@
+package dev.kotryos.minischeduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSchedulerApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/kotryos/minischeduler/MiniSchedulerApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.kotryos.minischeduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
